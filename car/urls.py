4125afa547b9403fe172ab1b4a6f7dcf6012decb
Denard Bazelli <denardbazelli@gmail.com>
2019-12-05 16:24:12 +0100

    issue 3 resolved (delete)

--- car/urls.py
+++ car/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:car_id>/', views.details),
     path('new/', views.new_car, name="new"),
     path('new/add/', views.add_car),
     path('delete_car/<int:car_id>/', views.delete_car),
+    path('edit/<int:car_id>/', views.get_car),
+    path('edit/<int:car_id>/new/', views.edit_car),
 ]
--- car/views.py
+++ car/views.py
@@ -10,6 +10,15 @@ def index(request):
     return render(request, "../templates/car/index", {"car_list": car_list, })
 
 
+def details(request, car_id):
+    car = Cars.objects.get(id=car_id)
+    context = {
+        'brand': car.brand,
+        'model': car.model,
+        'year': car.year
+    }
+    return render(request, "../templates/car/car_details.html", context)
+
 def new_car(request):
     form = Add_New_Car()
     return render(request, "../templates/car/new_car.html", {'form': form})
@@ -35,3 +44,26 @@ def add_car(request):
 def delete_car(request, car_id):
     Cars.objects.get(id=car_id).delete()
     return HttpResponseRedirect("/")
+
+
+def get_car(request, car_id):
+    form = Add_New_Car(request.POST)
+    return render(request, "../templates/car/edit_car.html", {"form": form})
+
+
+def edit_car(request, car_id):
+    if request.method == "POST":
+        form = Add_New_Car(request.POST)
+        if form.is_valid():
+            brand = form.cleaned_data["brand"]
+            model = form.cleaned_data["model"]
+            year = form.cleaned_data["year"]
+            edited_car = Cars.objects.get(id=car_id)
+            edited_car.brand = brand
+            edited_car.model = model
+            edited_car.year = year
+
+            edited_car.save()
+            return HttpResponseRedirect("/")
+    else:
+        return HttpResponseRedirect("/new/")
