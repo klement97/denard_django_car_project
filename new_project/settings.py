147baeabf42dcd0a062e822c58941df665b1e016
Klement Omeri <klement-omeri97@hotmail.com>
2019-12-23 22:46:13 +0100

    Prepare for Heroku Deployment

--- new_project/settings.py
+++ new_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -126,3 +128,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = '/car_list'
 LOGOUT_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
